eac819be03d382bbcc83ca3b7a56cc7528742e33
lequo <lequocthangbmt@gmail.com>
2024-09-07 14:15:29 +0700

    clean code fuction login and forget

--- src/main/java/Thuchanh/setup.java
+++ src/main/java/Thuchanh/setup.java
@@ -2,37 +2,53 @@ package Thuchanh;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
-import java.util.LinkedList;
-import java.util.List;
 
 public class setup {
-    protected WebDriver driver;
+    WebDriver driver = new ChromeDriver();
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(100));
+    public  String url = "https://hrm.anhtester.com/";
+    public  By username_ip = By.xpath("//input[@placeholder='Username']");
+    public  By password_ip = By.xpath("//input[@placeholder='Password']");
+    public  By login_btn = By.xpath("(//button[normalize-space()='Login'])[1]");
 
 
-    public static List<String> element(){
-        String url = "https://hrm.anhtester.com/";
-        By username_ip = By.xpath("(//input[@placeholder='Username'])[1]");
-        By password_ip = By.xpath("//input[@id='iusername']");
-        By login_btn = By.xpath("//input[@id='iusername']");
-        List<String>  listString = new LinkedList<>();
-        listString.add(username_ip.toString().replace("By.xpath: ", ""));
-        listString.add(password_ip.toString());
-        listString.add(login_btn.toString());
-        return listString;
+    public setup( ){
+
+    }
+
+    public void setups(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\lequo\\Downloads\\moi_truong_test\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        driver = new ChromeDriver(); // Khởi tạo driver
+        driver.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.manage().window().maximize();
     }
 
 
-    public void setups() {
-        this.driver = driver;
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\lequo\\Downloads\\moi_truong_test\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
-        driver = new ChromeDriver(); // Khởi tạo driver
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(50));
+    public void login(String username, String password) throws InterruptedException {
+        Thread.sleep(2000);
+        driver.findElement(username_ip).sendKeys(username);
+        Thread.sleep(2000);
+        driver.findElement(password_ip).sendKeys(password);
+        Thread.sleep(200);
+        driver.findElement(login_btn).click();
 
-        driver.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-        driver.manage().window().maximize();
+    }
+
+    public void Forgot_password() throws InterruptedException {
+        Thread.sleep(10000);
+        driver.findElement(By.xpath("(//p[@class='oxd-text oxd-text--p orangehrm-login-forgot-header'])[1]")).click();
+        Thread.sleep(10000);
+        driver.findElement(By.xpath("(//input[@placeholder='Username'])[1]")).sendKeys("Admin");
+        driver.findElement(By.xpath("(//button[normalize-space()='Cancel'])[1]")).click();
+    }
+
+    public void sercher(){
+
     }
 
 }
